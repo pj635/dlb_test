073a973d5d57062b5a504194490cbf2c34c09eeb
pj635 <2559394890@qq.com>
2022-01-26 14:48:19 +0800

    update

--- src/main/java/dlb/AesCBC.java
+++ src/main/java/dlb/AesCBC.java
@@ -1,72 +1,80 @@
-package com.runoob.dlb;
+package dlb;
 
-import javax.crypto.Cipher;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
-
-import org.apache.commons.codec.DecoderException;
-import org.apache.commons.codec.binary.Hex;
 
 import java.util.Arrays;
 import java.util.Base64;
-
-public class AesCTR {
-	
-	private static String ivHexStr = "31313131313131313131313131313131";
-	private static String keyHexStr = "31313131313131313131313131313131";
-//	private static String keyHexStr = "399b0686376d80318b872e61ec1b9c9c";
-//  private static String ivHexStr="3dafba429d9eb430b422da802c9fac41";
-	
-	private static byte[] keyByte = null;
-	private static byte[] ivByte = null;
-	
-	private static AesCTR instance=null;
-	
-	public AesCTR() throws DecoderException{
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+ 
+ 
+public class AesCBC {
+	private static String ivHexStr = "31313131313131313131313131313131"; //假设偏移为"1111111111111111"
+	private static String keyHexStr = "31313131313131313131313131313131"; //假设秘钥为"1111111111111111"
+    private static byte[] keyByte = null;
+    private static byte[] ivByte = null;
+    
+    private static AesCBC instance=null;
+    
+ 
+    public AesCBC() throws DecoderException{
     	keyByte = Hex.decodeHex(keyHexStr.toCharArray());
     	ivByte = Hex.decodeHex(ivHexStr.toCharArray());
     }
-	
-	public static AesCTR getInstance() throws DecoderException {
+ 
+ 
+    public static AesCBC getInstance() throws DecoderException {
         if (instance==null)
-            instance= new AesCTR();
+            instance= new AesCBC();
         return instance;
     }
-	
-	// 加密
+ 
+
+    // 加密
     public byte[] encrypt(String sSrc, byte[] keyByte, byte[] ivByte) throws Exception {
-    	Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");	
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        System.out.println("加密算法是：" + cipher.getAlgorithm());
         SecretKeySpec skeySpec = new SecretKeySpec(keyByte, "AES");
-        IvParameterSpec iv = new IvParameterSpec(ivByte);
+        IvParameterSpec iv = new IvParameterSpec(ivByte);//使用CBC模式，需要一个向量iv，可增加加密算法的强度
         cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
         byte[] encrypted = cipher.doFinal(sSrc.getBytes("utf-8"));
         return encrypted;
     }
-	
+ 
+ 
 	// 解密
 	public String decrypt(byte[] encrypted, byte[] keyByte, byte[] ivByte) throws Exception {
 		SecretKeySpec skeySpec = new SecretKeySpec(keyByte, "AES");
-		Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
-		System.out.println("加密算法是：" + cipher.getAlgorithm());
+		Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
 		IvParameterSpec iv = new IvParameterSpec(ivByte);
 		cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
 		byte[] original = cipher.doFinal(encrypted);
 		String originalString = new String(original, "utf-8");
 		return originalString;
 	}
+ 
     
-	public static void main(String[] args) throws Exception {
-		// 需要加密的字串
+    public static void main(String[] args) throws Exception {
+        // 需要加密的字串
         String cSrc = "123456";
-        System.out.println("加密前的字串是："+ cSrc);
+        System.out.println("加密前的字串是："+cSrc);
         // 加密
-        byte[] encrypted = AesCTR.getInstance().encrypt(cSrc, keyByte, ivByte);
+        byte[] encrypted = AesCBC.getInstance().encrypt(cSrc, keyByte, ivByte);
         System.out.println("加密后的结果是(byte[])："+ Arrays.toString(encrypted));
         System.out.println("加密后的结果是(base64)："+ Base64.getEncoder().encodeToString(encrypted));
         
         // 解密
-        String DeString = AesCTR.getInstance().decrypt(encrypted, keyByte, ivByte);
+        String DeString = AesCBC.getInstance().decrypt(encrypted, keyByte, ivByte);
         System.out.println("解密后的字串是：" + DeString);
-	}
-
+    }
+    
 }
+
+
+
+
+
+
+
--- src/test/java/dlb_test/TestDLB.java
+++ src/test/java/dlb_test/TestDLB.java
@@ -1,5 +1,7 @@
-package com.runnoob.dlb.test;
+package dlb_test;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
@@ -7,73 +9,77 @@ import java.net.MulticastSocket;
 //import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.Map;
 
 import org.apache.commons.codec.binary.Hex;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.yaml.snakeyaml.Yaml;
 
-import com.runoob.dlb.AesCTR;
+import dlb.AesCTR;
 
 public class TestDLB {
-	private static String keyHexStr = "399b0686376d80318b872e61ec1b9c9c";
-	private static String ivHexStr = "00000000000000000000000000000000";
 	private static String udpGroupServerIp = "233.0.0.6";
 	private static int udpGroupServerPort = 6000;
-	private MulticastSocket socket = null; //组播套接字
+	private MulticastSocket socket = null; // 组播套接字
 	private InetAddress group = null;
-	
+
 	@SuppressWarnings("deprecation")
 	@BeforeClass
-	public void setup_class() throws Exception {
-		group = InetAddress.getByName(udpGroupServerIp);// 组播地址
-		
+	public void setup_class() throws Exception {	
+		Yaml yaml = new Yaml(); // 初始化Yaml解析器
+		File f = new File("config.yaml");
+		Map<?, ?> result = (Map<?, ?>) yaml.load(new FileInputStream(f));
+
+		System.out.println(result.getClass());
+		System.out.println(result);
+
+		String keyHexStr = (String) ((Map<?, ?>) result.get("udpGroup")).get("keyHexStr");
+		String ivHexStr = (String) ((Map<?, ?>) result.get("udpGroup")).get("ivHexStr");
+		System.out.println("keyHexStr:" + keyHexStr);
+		System.out.println("ivHexStr:" + ivHexStr);
+
+		group = InetAddress.getByName(udpGroupServerIp); // 组播地址
 		socket = new MulticastSocket(udpGroupServerPort);
 		socket.joinGroup(group);// 加入连接
 		byte[] buffer = new byte[8192];
 		System.out.println("接收数据包启动！(启动时间: " + new Date() + ")");
-		while (true) {
-			// 建立一个指定缓冲区大小的数据包
-			DatagramPacket dp = new DatagramPacket(buffer, buffer.length);
-			socket.receive(dp);
-			byte[] encrypted = dp.getData();
-			System.out.println("收到的组播数据（加密）（byte[]）：" + Arrays.toString(encrypted));
 
-			// 对收到的组播数据进行解密
-			byte[] keyByte = null;
-			byte[] ivByte = null;
+		// 建立一个指定缓冲区大小的数据包
+		DatagramPacket dp = new DatagramPacket(buffer, buffer.length);
+		socket.receive(dp);
+		byte[] encrypted = dp.getData();
+		System.out.println("收到的组播数据（加密）（byte[]）：" + Arrays.toString(encrypted));
 
-			keyByte = Hex.decodeHex(keyHexStr.toCharArray());
-			ivByte = Hex.decodeHex(ivHexStr.toCharArray());
+		// 对收到的组播数据进行解密
+		byte[] keyByte = null;
+		byte[] ivByte = null;
 
-			// 解密
-			String DeString = AesCTR.getInstance().decrypt(encrypted, keyByte, ivByte);
-			System.out.println("解密后的字串是：" + DeString);
-		}
+		keyByte = Hex.decodeHex(keyHexStr.toCharArray());
+		ivByte = Hex.decodeHex(ivHexStr.toCharArray());
+
+		// 解密
+		String DeString = AesCTR.getInstance().decrypt(encrypted, keyByte, ivByte);
+		System.out.println("解密后的字串是：" + DeString);
 	}
-	
-	
+
 	@SuppressWarnings("deprecation")
 	@AfterClass
 	public void teardown_class() throws Exception {
 		if (socket != null) {
-			try {
-				socket.leaveGroup(group);
-				socket.close();
-			} catch (Exception e2) {
-				System.out.println("leave group or close socket error");
-			}
+			socket.leaveGroup(group);
+			socket.close();
 		}
 	}
-	
-	
-	@Test 
+
+	@Test
 	public void test_1() throws Exception {
 		int responseCode = 200;
-		
+
 		Assert.assertEquals(responseCode, 200, "The response code should be 200!");
-		
+
 //		InetAddress group = InetAddress.getByName(udpGroupServerIp);// 组播地址
 //		MulticastSocket socket = null;// 创建组播套接字
 //		try {
